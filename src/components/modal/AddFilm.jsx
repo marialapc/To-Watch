266cf1ca69533213559896c57c42dc1f1cedb2b8
marialapc <marialopez.apc@gmail.com>
2023-05-28 13:26:42 +0200

    modal prevent default

--- src/components/modal/AddFilm.jsx
+++ src/components/modal/AddFilm.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import './addFilm.scss';
+import "./addFilm.scss";
 
-function AddFilm () {
-    return (
-        <form className="form">
-            <label className="form__label">Escribe tu nueva pelicula</label>
-            <textarea className="form__textarea" placeholder="Blade Runner"></textarea>
-            <div className="form__buttonContainer">
-            <button className=" form__button form__button--cancel">CANCEL</button>
-            <button className=" form__button form__button--add">ADD</button>
-            </div>
-        </form>
-    );
+function AddFilm() {
+  return (
+    <form className="form" onSubmit={(event) => {
+      event.preventDefault();
+    }}>
+      <label className="form__label">Escribe tu nueva pelicula</label>
+      <textarea
+        className="form__textarea"
+        placeholder="Blade Runner"
+      ></textarea>
+      <div className="form__buttonContainer">
+        <button className=" form__button form__button--cancel">CANCEL</button>
+        <button className=" form__button form__button--add">ADD</button>
+      </div>
+    </form>
+  );
 }
 
-export {AddFilm};
+export { AddFilm };
