8cf846c26fc514f509ce87a7ddd1672bd5fa20f0
marialapc <marialopez.apc@gmail.com>
2023-05-28 13:31:16 +0200

    cancel submit

--- src/components/modal/AddFilm.jsx
+++ src/components/modal/AddFilm.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import "./addFilm.scss";
+import { TowatchContext } from "../../hooks/TowatchContext";
 
 function AddFilm() {
+    const {
+     setOpenModal,
+     
+    } = React.useContext(TowatchContext);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setOpenModal(false);
+      };
+
+      const onCancel =() => {
+        setOpenModal(false); 
+      }
+
   return (
-    <form className="form" onSubmit={(event) => {
-      event.preventDefault();
-    }}>
+    <form className="form" onSubmit={onSubmit}>
       <label className="form__label">Escribe tu nueva pelicula</label>
       <textarea
         className="form__textarea"
         placeholder="Blade Runner"
       ></textarea>
       <div className="form__buttonContainer">
-        <button className=" form__button form__button--cancel">CANCEL</button>
+        <button className=" form__button form__button--cancel" onClick={onCancel}>CANCEL</button>
         <button className=" form__button form__button--add">ADD</button>
       </div>
     </form>
