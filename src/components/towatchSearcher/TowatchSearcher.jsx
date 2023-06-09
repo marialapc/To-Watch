9a51e024ce38005f6c14f84a74b0ac3622a99f08
marialapc <marialopez.apc@gmail.com>
2023-06-09 08:21:09 +0200

    change search bar

--- src/components/towatchSearcher/TowatchSearcher.jsx
+++ src/components/towatchSearcher/TowatchSearcher.jsx
@@ -1,7 +1,6 @@
 import React  from "react";
 import "./towatchSearcher.scss"
 import { TowatchContext } from "../../hooks/TowatchContext";
-import searchIcon from "../../assets/icons/search-icon.png"
 
 function TowatchSearcher(){
   const {
@@ -10,10 +9,9 @@ function TowatchSearcher(){
    } = React.useContext(TowatchContext)
     return(
       <div className="towatchSearcher">
-      <img  className="towatchSearcher__icon"  src={searchIcon} alt="search" />
      <input 
      className="towatchSearcher__bar" 
-     placeholder="...The third man"
+     placeholder="       ...The third man"
      value={searchValue}
      onChange={(event) =>{
        setSearchValue(event.target.value);
